e212daa19bb3f035c86b36b43990364e3195e2d9
xxl-007 <1186479152@qq.com>
2018-11-30 09:36:42 +0800

    添加api,server

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+
+// 首页页面数据请求：https://b2capigateway.yiguo.com/api/home/TemplateComponent/GetTemplateComponentInfo
+const HOME_API="/api/home/TemplateComponent/GetTemplateComponentInfo";
+
+
+export default {
+    HOME_API
+}
---
+++ src/services/index.jsx
@@ -0,0 +1,6 @@
+import API from "../api"
+
+//请求首页数据
+export function getHomeData(){
+    
+}
